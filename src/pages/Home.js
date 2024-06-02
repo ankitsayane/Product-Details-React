53b771a25533c433006c7d7687ac944e6bedd0a6
ankitsayane <ankitsayane256@gmail.com>
2024-06-02 14:22:24 +0530

    Modified Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
-import img from "../Images/home.jpg";
+import img from "../Images/sale.jpg";
 
 export default function Home() {
   return (
-    <>
-      <div class="text-bg-secondary p-3">This is Home Page</div>
-    </>
+    <div className="d-flex flex-column align-items-center">
+      <h1 className="mt-5">React Product Management</h1>
+      <img
+        src={img}
+        className="img-fluid mt-3"
+        alt="Sale"
+        style={{ maxWidth: "80%", height: "auto", maxHeight: "60vh" }}
+      />
+    </div>
   );
 }
